dc0d9279b6da2c663db3e9202e8054d5c4bce5ad
HJKCC <chencheng0816@gmail.com>
2019-04-26 11:28:12 +0800

    modified mybatis-generator

--- exchange-ui/src/layouts/index.js
+++ exchange-ui/src/layouts/index.js
@@ -1,11 +1,13 @@
+import LoginForm from '../pages/login';
 import styles from './index.css';
 
 function BasicLayout(props) {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+      <h1 className={styles.title}>Welcome to BlockChain-Exchange!</h1>
       {props.children}
     </div>
+
   );
 }
 
--- exchange-ui/src/pages/index.js
+++ exchange-ui/src/pages/index.js
@@ -8,8 +8,8 @@ export default function() {
       <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
+          <a href="login">
+            Go to Login!
           </a>
         </li>
       </ul>
